402b610fe55fd3dc065ec5abe603621fc7fbac1f
Sachin <Sachin@192.168.0.15>
2016-11-14 20:08:02 -0500

    Checkout Class that prints processing payment message

---
+++ src/com/KST/eCommerce/Checkout.java
@@ -0,0 +1,16 @@
+package com.KST.eCommerce;
+
+/**
+ *
+ * @author Kureishi Shivanand
+ */
+public class Checkout {
+    
+    /**
+     * Prints processing message
+     * 
+     */
+    public void processPayment() {
+        System.out.println("Payment is being processed");
+    }
+}
