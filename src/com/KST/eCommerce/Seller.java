6e38b27b9790bdaae716224e1a2904e10a15f30a
ktsuench <will289@hotmail.com>
2016-11-23 01:32:32 -0500

    updated seller class

--- src/com/KST/eCommerce/Seller.java
+++ src/com/KST/eCommerce/Seller.java
@@ -9,18 +9,21 @@ import java.util.ArrayList;
 public class Seller extends User implements ItemList {
 
     // Instance Variables
+    private final int id;
     private final String password;
-    private ArrayList <Item> items; 
+    private ArrayList<Item> items;
+
     /**
      * Class Constructor
      *
      * @param name
      * @param password
      */
-    public Seller(String name, String password) {
+    public Seller(String name, int id, String password) {
         super(name, User.UserRole.seller);
+        this.id = id;
         this.password = password;
-        ArrayList<Item> items = new ArrayList(); 
+        ArrayList<Item> items = new ArrayList();
     }
 
     /**
@@ -32,18 +35,15 @@ public class Seller extends User implements ItemList {
     public boolean validPassword(String password) {
         return this.password.equals(password);
     }
-    
-     @Override
-    public void addItem(Item item){
+
+    @Override
+    public void addItem(Item item) {
         items.add(item);
     }
-    
-    
+
     @Override
-    public void removeItem(Item item){ 
-        items.remove(item); 
+    public void removeItem(Item item) {
+        items.remove(item);
     }
-    
-    
-    
+
 }
