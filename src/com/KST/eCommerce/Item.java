77655d49886128f3135bcadbe10e15cea4bda433
ktsuench <will289@hotmail.com>
2016-11-24 03:13:56 -0500

    added in equals method, used for unit testing

--- src/com/KST/eCommerce/Item.java
+++ src/com/KST/eCommerce/Item.java
@@ -10,36 +10,53 @@ package com.KST.eCommerce;
  * @author Tejveer
  */
 public class Item {
+
     //Instance Variables
-    private final int id; 
-    private final String title; 
-    private final String description; 
-    private final double price; 
-    
-    
+    private final int id;
+    private final String title;
+    private final String description;
+    private final double price;
+
     //Constructor
-    public Item(int id, String title, String description, double price){ 
-        this.id = id; 
-        this.title = title; 
-        this.description = description; 
-        this.price = price; 
-        
+    public Item(int id, String title, String description, double price) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.price = price;
+
     }
-    
+
     //Methods
-    public int getId(){ 
-        return this.id; 
+    public int getId() {
+        return this.id;
     }
-    
-    public String getTitle(){ 
-        return this.title; 
+
+    public String getTitle() {
+        return this.title;
     }
-    
-    public String getDescription(){ 
-        return this.description; 
-    }    
-    public double getPrice(){ 
-        return this.price; 
-        
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public double getPrice() {
+        return this.price;
+
+    }
+
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        if (o instanceof Item) {
+            Item t = (Item) o;
+
+            if (t.id != this.id) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- src/com/KST/eCommerce/Seller.java
+++ src/com/KST/eCommerce/Seller.java
@@ -11,19 +11,29 @@ public class Seller extends User implements ItemList {
     // Instance Variables
     private final int id;
     private final String password;
-    private ArrayList<Item> items;
+    private final ArrayList<Item> items;
 
     /**
      * Class Constructor
      *
      * @param name
+     * @param id
      * @param password
      */
     public Seller(String name, int id, String password) {
         super(name, User.UserRole.seller);
         this.id = id;
         this.password = password;
-        ArrayList<Item> items = new ArrayList();
+        this.items = new ArrayList();
+    }
+
+    /**
+     * Returns user ID
+     *
+     * @return int
+     */
+    public int getId() {
+        return this.id;
     }
 
     /**
@@ -46,4 +56,23 @@ public class Seller extends User implements ItemList {
         items.remove(item);
     }
 
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+
+        if (o instanceof Seller) {
+            Seller s = (Seller) o;
+
+            if (s.items.size() == this.items.size()) {
+                for (int i = 0; i < s.items.size(); i++) {
+                    if (!s.items.get(i).equals(this.items.get(i))) {
+                        return false;
+                    }
+                }
+            }
+        }
+        
+        return true;
+    }
 }
