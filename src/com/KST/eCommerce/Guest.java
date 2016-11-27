bce7d3c5654c7a5a1e6d36fe03cce00e177223fd
ktsuench <will289@hotmail.com>
2016-11-27 03:47:53 -0500

    added in new constructor and equals method

--- src/com/KST/eCommerce/Guest.java
+++ src/com/KST/eCommerce/Guest.java
@@ -8,10 +8,32 @@ public class Guest extends User {
 
     /**
      * Class Constructor
+     */
+    public Guest() {
+        super("Guest", User.UserRole.guest);
+    }
+
+    /**
+     * Class Constructor
      *
      * @param name
      */
     public Guest(String name) {
         super(name, User.UserRole.guest);
+    }
+    
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof Guest) {
+            Guest g = (Guest) o;
+            
+            if (g.getName() != this.name) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+        
+        return true;
     }
 }
