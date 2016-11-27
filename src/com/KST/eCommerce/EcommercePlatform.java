9bd305557b44a84aa2b615f2b46b368b6991c27b
ktsuench <will289@hotmail.com>
2016-11-26 19:46:02 -0500

    added comments

--- src/com/KST/eCommerce/EcommercePlatform.java
+++ src/com/KST/eCommerce/EcommercePlatform.java
@@ -10,10 +10,14 @@ import java.util.logging.Logger;
  */
 public class EcommercePlatform {
 
+    // Instance Variables
     private final Session session;
     private final PaymentProcessor checkout;
     private final ArrayList<User> users;
 
+    /**
+     * Class Constructor
+     */
     public EcommercePlatform() {
         Database db = new Database("users.data");
 
@@ -24,6 +28,11 @@ public class EcommercePlatform {
         db.closeDb();
     }
 
+    /**
+     * Retrieve the list of store items
+     *
+     * @return ArrayList<Item>
+     */
     public ArrayList<Item> listItems() {
         ArrayList<Item> items = new ArrayList<>();
 
@@ -36,31 +45,51 @@ public class EcommercePlatform {
         return (ArrayList<Item>) items.clone();
     }
 
+    /**
+     * Retrieve current session
+     *
+     * @return Session
+     */
     public Session getSession() {
         return this.session;
     }
-    
+
+    /**
+     * Tries to login with given name and password. On failure returns false
+     * otherwise returns true.
+     *
+     * @param name
+     * @param password
+     * @return boolean
+     */
     public boolean login(String name, String password) {
         try {
             return this.session.login(this.users, name, password);
         } catch (Exception ex) {
             Logger.getLogger(EcommercePlatform.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         return false;
     }
-    
+
+    /**
+     * Retrieves the payment processor.
+     * @return 
+     */
     public PaymentProcessor getPaymentProcessor() {
         return this.checkout;
     }
-    
+
+    /**
+     * Shows the GUI interface for the application.
+     */
     public void showInterface() {
         EcommerceGUI.showGUI(this);
     }
 
     public static void main(String[] args) {
         EcommercePlatform platform = new EcommercePlatform();
-        
+
         platform.showInterface();
     }
 }
