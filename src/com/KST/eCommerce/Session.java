4356e0df9cccdec1f359d7628cbf4ee122e442c2
ktsuench <will289@hotmail.com>
2016-11-27 02:49:51 -0500

    updated abstraction function and rep invariant updated methods specification clauses and implementations

--- src/com/KST/eCommerce/Session.java
+++ src/com/KST/eCommerce/Session.java
@@ -9,19 +9,21 @@ import java.util.ArrayList;
 public class Session {
 
     /**
-     * OVERVIEW: Sessions are mutable. When a user is able to login, they have
-     * the options of adding/removing items to their cart or adding/removing
-     * items to the store, depending on whether the user is a guest or seller,
-     * respectively.
+     * OVERVIEW: Sessions are mutable. Users have the options of adding/removing
+     * items to their cart or adding/removing items to the store, depending on
+     * whether the user is a guest or seller, respectively. Sellers need to
+     * login.
      */
     
-    // Abstraction function is
-    //          c.cart = cart
+    // Abstraction function, AF(c) is
+    // For user u
+    // c.cart -> u.shoppingCart
+    // c.isLoggedIn -> u.role == guest if false, u.role == seller if true
     
     // The rep invariant is:
-    // c.cart != null
-    
-//Instance Variables
+    // c.user != null
+
+    //Instance Variables
     private boolean isLoggedIn;
     private User user;
     private ItemCart cart;
@@ -31,17 +33,17 @@ public class Session {
      */
     public Session() {
         // MODIFIES: isLoggedIn, user, cart
-        // EFFECTS: initializes isLoggedIn to false, user as a null array, cart to new object (default)
+        // EFFECTS: initializes isLoggedIn to false, user as a guest, cart to new object (default)
 
         isLoggedIn = false;
-        user = null;
+        user = new Guest("Guest");
         cart = new ItemCart();
     }
 
     public User getUser() {
-        return ((Seller) user);
+        return user;
     }
-    
+
     /**
      * Checks if login information is valid and logs user in
      *
@@ -53,13 +55,18 @@ public class Session {
      */
     public boolean login(ArrayList<User> users, String name, String password) throws Exception {
         // REQUIRES: users != null, name != null, password != null
-        // MODIFIES: users, isLoggedIn
-        // EFFECTS: throws Exception if there are no users. If their is a user, checks if 
-        // the name and password of that user exists in the predefined list. Then sets the 
+        // MODIFIES: users, isLoggedIn, cart
+        // EFFECTS: throws Exception if there are no users or no login
+        // information is provided. If their is a user, checks if the name and
+        // password of that user exists in the predefined list. Then sets the 
         // role (guest/seller) as defined in the list to the current user
 
         if (users == null) {                                            // if their are no User objects
             throw new Exception("No users");
+        }
+        
+        if (name == null || password == null) {
+            throw new Exception("No login information provided.");
         }
 
         int i = -1;
@@ -67,13 +74,16 @@ public class Session {
 
         // if user is not logged in
         if (isLoggedIn == false) {
-            while (isLoggedIn != true && ++i < users.size()) {           // and the number of login requests is less than the amount of users listed
-                tempUser = (User) users.get(i);                          // tempUser assigned to current position of user in the list of users
-                if (tempUser.getName().equals(name)) {                   // if the name in param MATHCES one of the name after tempUser runs through list
-                    if (((Seller) tempUser).validPassword(password)) {   // if the password of tempUser of THAT name matches the password in param
-                        isLoggedIn = true;                               // user able to log in
-                        user = tempUser;                                 // set tempUser to an ACTUAL user
-                        return true;
+            while (isLoggedIn != true && ++i < users.size()) {              // and the number of login requests is less than the amount of users listed
+                tempUser = (User) users.get(i);                              // tempUser assigned to current position of user in the list of users
+                if (tempUser.getName().equals(name)) {                      // if the name in param MATHCES one of the name after tempUser runs through list
+                    if (tempUser instanceof Seller) {
+                        if (((Seller) tempUser).validPassword(password)) {   // if the password of tempUser of THAT name matches the password in param
+                            isLoggedIn = true;                               // user able to log in
+                            user = tempUser;                                 // set tempUser to an ACTUAL user
+                            cart = null;
+                            return true;
+                        }
                     }
                 }
             }
@@ -87,15 +97,17 @@ public class Session {
      * @return boolean
      */
     public boolean logout() {
-        // REQUIRES: user != null
-        // MODIFIES: user, isLoggedIn
+        // REQUIRES: isLoggedIn = true
+        // MODIFIES: user, isLoggedIn, cart
         // EFFECTS: logs the user out after they finish their session. Reset variables to default
 
         if (isLoggedIn) {
-            user = null;
+            user = new Guest("Guest");
             isLoggedIn = false;
+            cart = new ItemCart();
             return true;
         }
+        
         return false;
     }
 
@@ -106,12 +118,19 @@ public class Session {
      * @return String
      */
     public String purchase(PaymentProcessor checkout) {
+        // REQUIRES: checkout != null, user instanceof Guest, size of cart > 0
         // MODIFIES: cart
         // EFFECTS: Returns checkout message at the end of session and empties the cart for next user
 
-        String result = checkout.processPayment();
-        cart = new ItemCart();
-
+        String result;
+        
+        if (user instanceof Guest && checkout != null && cart.getSizeOfCart() > 0) {
+            result = checkout.processPayment();
+            cart = new ItemCart();
+        } else {
+            result = "Failed to process payment.";
+        }
+        
         return result;
     }
 
@@ -121,12 +140,14 @@ public class Session {
      * @param item
      */
     public void addToCart(Item item) {
-        // REQUIRES: item != null
+        // REQUIRES: item != null, cart != null, and user instanceof Guest
         // MODIFIES: cart   
         // EFFECTS: adds the specified item to the cart
         //          i.e. cart_post = cart + {item}
 
-        cart.addItem(item);
+        if (user instanceof Guest && cart != null && item != null) {
+            cart.addItem(item);
+        }
     }
 
     /**
@@ -135,13 +156,21 @@ public class Session {
      * @param item
      * @return boolean
      */
-    public void removeFromCart(Item item) {
-        // REQUIRES: item != null
+    public boolean removeFromCart(Item item) {
+        // REQUIRES: user instance of Guest, cart != null, item != null, and
+        //           cart contains item
         // MODIFIES: cart
         // EFFECTS: removes the specified item from the cart
         //          i.e. cart_post = cart - {item}
 
-        cart.removeItem(item);
+        if (user instanceof Guest && cart != null && item != null) {
+            if (cart.containsItem(item)) {
+                cart.removeItem(item);
+                return true;
+            }
+        }
+        
+        return false;
     }
 
     /**
@@ -150,8 +179,14 @@ public class Session {
      * @return int
      */
     public int getCartSize() {
+        // REQUIRES: user instance of Guest and cart != null
         // EFFECTS: returns the size of the cart
-        return cart.getSizeOfCart();
+        
+        if (user instanceof Guest && cart != null) {
+            return cart.getSizeOfCart();
+        }
+        
+        return -1;
     }
 
     /**
@@ -160,10 +195,16 @@ public class Session {
      * @return ItemCart
      */
     public ItemCart getCart() {
+        // REQUIRES: user instanceof Guest and cart != null
         // EFFECTS: returns the item cart
-        return (ItemCart) cart.clone();
+        
+        if (user instanceof Guest && cart != null) {
+            return (ItemCart) cart.clone();
+        }
+        
+        return null;
     }
-
+    
     /**
      * Add the idem to the store
      *
@@ -171,15 +212,16 @@ public class Session {
      * @return boolean
      */
     public boolean addItemToStore(Item item) {
-        // REQUIRES: item != null
-        // MODIFIES: cart   
+        // REQUIRES: item != null user instanceof Seller
+        // MODIFIES: cart
         // EFFECTS: adds the specified item to the store
         //          i.e. (Seller)user_post = (Seller)user + {item}
-        
-        if (isLoggedIn == true) {
+
+        if (user instanceof Seller && item != null) {
             ((Seller) user).addItem(item);
             return true;
         }
+        
         return false;
     }
 
@@ -190,17 +232,17 @@ public class Session {
      * @return boolean
      */
     public boolean removeItemFromStore(Item item) {
-        // REQUIRES: user != null
+        // REQUIRES: user instanceof Seller and item != null
         // MODIFIES: cart   
         // EFFECTS: removes the specified item from the store
         //          i.e. (Seller)user_post = (Seller)user - {item}
 
-        if(item == null){
-            return false;
-        } else {
+        if (user instanceof Seller && item != null) {
             ((Seller) user).removeItem(item);
             return true;
         }
+        
+        return false;
     }
 
     /**
@@ -209,14 +251,15 @@ public class Session {
      * @return ArrayList
      */
     public ArrayList<Item> getItemStore() {
-        // REQUIRES: user != null
-        // EFFECTS: returns the items of the seller
-        
-        if(user != null) {
+        // REQUIRES: user instanceof Seller
+        // EFFECTS: returns the items of the seller if user is a seller
+        //          otherwise returns null
+
+        if (user instanceof Seller) {
             return ((Seller) user).getItems();
-        } else {
-            return null;
         }
+        
+        return null;
     }
 
     /**
@@ -229,6 +272,7 @@ public class Session {
         if (user == null) {
             return false;
         }
+        
         return true;
     }
 
@@ -240,10 +284,15 @@ public class Session {
     @Override
     public String toString() {
         if (repOK() == true) {
-            return "Valid";
-        }
-        else {
-            return "Invalid";
+            String result = "User: " + user.getRole().toString();
+            
+            for(Item i : cart.getItems()) {
+                result += "\n" + i.toString();
+            }
+            
+            return result;
+        } else {
+            return "Invalid rep invariant.";
         }
     }
 }
